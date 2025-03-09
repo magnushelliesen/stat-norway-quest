72fbe418949a4da8c72d5cd986606d8cd8858f24
magnushelliesen <magnus.helliesen@gmail.com>
2025-03-09 09:14:24 +0100

    Make window wait in loop

--- src/window.cpp
+++ src/window.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 // Window properties
-const int WIDTH = 800;
-const int HEIGHT = 600;
+const int WIDTH = 1900;
+const int HEIGHT = 1200;
 
 int window(){
     // Initialize SDL
@@ -14,12 +14,28 @@ int window(){
     }
 
     // Create window
-    SDL_Window* window = SDL_CreateWindow("Cross-Platform Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("StatNorwayQuest", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
         SDL_Quit();
         return 1;
     }
 
+    // **Event loop to keep the window open**
+    bool running = true;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {  // If the user clicks the close button
+                running = false;
+            }
+        }
+        SDL_Delay(16); // Prevents high CPU usage (approx. 60 FPS)
+    }
+
+    // Cleanup
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    
     return 0;
 }
