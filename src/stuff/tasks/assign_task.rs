d0cd7ff63db3719783a5b1e1b8c4dda9bd457aad
magnushelliesen <magnus.helliesen@gmail.com>
2026-01-18 07:58:30 +0100

    Refaktor

---
+++ src/stuff/tasks/assign_task.rs
@@ -0,0 +1,17 @@
+use rand::random_range;
+
+pub fn assign_task(tenure: i32) -> fn() -> bool {
+    // Make a task index that based on tenure and randomness
+    let task_index = tenure + random_range(1..100);
+
+    // Return task based on tenure
+    if task_index <= 1 {
+        fix_model
+    } else {
+        fix_model
+    }
+}
+
+pub fn fix_model() -> bool {
+    true
+}
